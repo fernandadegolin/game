90f71f639f3970817f7105d3e617fec0a9855451
Fernanda Degolin <57374044+fernandadegolin@users.noreply.github.com>
2020-06-24 17:48:03 -0300

    Add files via upload

---
+++ scripts/jogo/personagem.js
@@ -0,0 +1,14 @@
+class Personagem {
+  constructor(imagem){
+    this.imagem = imagem;
+    this.matriz = 
+  }
+
+  exibe(){
+  image(this.imagem, 0, height -135, 110, 135, 0, 0, 220, 270);
+  }
+
+  anima(){
+  
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,52 @@
+let imagemCenario;
+let imagemPersonagem;
+let cenario;
+let somDoJogo;
+let personagem;
+
+    
+function preload(){
+  imagemCenario = loadImage('imagens/cenario/floresta.png');
+  imagemPersonagem = loadImage('imagens/personagem/correndo.png');
+  somDoJogo = loadSound('sons/trilha_jogo.mp3');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  cenario = new Cenario (imagemCenario, 1);
+  personagem = new Personagem (imagemPersonagem);
+  //somDoJogo.loop();
+  
+}
+
+function draw() {
+  cenario.exibe();
+  //cenario.move();
+  
+}
+
+class Cenario {
+  constructor(imagem, velocidade){
+    this.imagem = imagem;
+    this.velocidade = velocidade;
+    this.x1 = 0;
+    this.x2 = width;
+  }
+  
+  exibe(){
+    image(this.imagem, this.x1, 0, width, height);
+    image(this.imagem, this.x2, 0, width, height);
+  }
+  
+  move(){
+    this.x1 = this.x1 - this.velocidade;
+    this.x2 = this.x2 - this.velocidade;
+  
+    if (this.x1 < - width){
+       this.x1 = width;
+    } 
+    if (this.x2 < -width){
+    this.x2 = width;
+  }
+}
+}
