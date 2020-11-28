bd5ba26f1315632eaefc3daf78cf8b1fa69a1287
Fernanda Degolin <57374044+fernandadegolin@users.noreply.github.com>
2020-11-28 09:31:41 -0300

    Update load.js

--- scripts/load.js
+++ scripts/load.js
@@ -8,9 +8,7 @@ function preload(){
   imagemGameOver = loadImage('imagens/assets/game-over.png');
   imagemCenario = loadImage('imagens/cenario/floresta.png');
   imagemPersonagem = loadImage('imagens/personagem/correndo.png');
-  
-  
-  imagemInimigo = loadImage('imagens/inimigos/gotinha.png');
+    imagemInimigo = loadImage('imagens/inimigos/gotinha.png');
   imagemInimigoGrande = loadImage ('imagens/inimigos/troll.png');
   imagemInimigoVoador = loadImage ('imagens/inimigos/gotinha-voadora.png');
   fita = loadJSON('fita/fita.json');
